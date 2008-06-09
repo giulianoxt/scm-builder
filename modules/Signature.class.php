1179747f965f2a00ad80c2abe19eb18c49d53201
Giuliano Vilela <giulianoxt@gmail.com>
2008-06-09 22:50:54 +0000

    Completando e ajeitando os comentários. Tudo em português ainda.

--- modules/Signature.class.php
+++ modules/Signature.class.php
@@ -1,4 +1,13 @@
 <?php
+	/**
+	* Signature.class.php
+	*
+	* @author Max Rosan
+	* @author Thales Galdino
+	* @author Giuliano Vilela
+	* @author Lucas Araújo
+	*/
+
 	error_reporting(E_ALL);
 
 	require_once("Node.class.php");
@@ -11,24 +20,34 @@
 	require_once("formulaConverter2.class.php");
 
 	/**
-	* @author Max Rosan
-	* @author Thales Galdino
-	* @author Giuliano Vilela
-	* @author Lucas Araújo
+	* class Signature.
+	* Responsável por extrair a assinatura mínima
+	* de uma fórmula qualquer.
 	*/
-
 	class Signature {
+		/**
+		* Construtor recebe uma fórmula e já processa, pegando a assinatura.
+		* @param $formula Objeto Node, representando a raiz da árvore da fórmula.
+		*/
 		public function Signature($formula) {
 			$this->signature = array();
 			$this->getSignature($formula);
 		}
 
+		/**
+		* Método interno para extrair a assinatura.
+		* Analisa o nó atual e recursivamente observa os seus filhos.
+		* @param $formula Objeto Node, representando a raiz da árvore da fórmula atual.
+		*/
 		public function getSignature($formula) {
+			// Caso o nó atual represente um elemento que pertença
+			// à assinatura mínima relativa à fórmula
 			if (($formula->content instanceof Constant) or
 			    ($formula->content instanceof Variable && !$formula->content->isLinked) or
 			    ($formula->content instanceof Func) or
 			    ($formula->content instanceof Relation)) {
 
+				// Vê se o elemento já tinha saido adicionado antes
 				$exists = false;
 				foreach ($this->signature as $element) {
 					if ($element->content == $formula->content->content) {
@@ -37,14 +56,20 @@
 					}
 				}
 
+				// Caso não tenha sido, adiciona-o à assinatura da fórmula
 				if (!$exists) $this->signature[] = $formula->content;
 			}
 
+			// Recursivamente analisa os filhos do nó atual
 			foreach ($formula->children as $value) {
 				$this->getSignature($value);
 			}
 		}
 	
+		/**
+		* Função pra debug.
+		* Imprime a assinatura final da fórmula passada pelo construtor.
+		*/
 		public function printSignature() {
 			echo "Tamanho = ".sizeof($this->signature)."<br>";
 			foreach ($this->signature as $value) {
@@ -57,14 +82,4 @@
 		public $signature;
 	}
 
-// 	$formula = 'R(x) & Y(c)';
-// 	$converter = new formulaConverter("T","");
-// 	$tree = $converter->infixToTree($formula,true);
-// 
-// 	$sig = new Signature($tree);
-// 
-// 	echo "Formula: ".$formula."<br><br>";
-// 
-// 	$sig->printSignature();
 ?>
-	
